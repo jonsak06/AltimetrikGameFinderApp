3515f0f9b7775f74bd9476c37463ba9aa9fb0af0
jonsak06 <64983860+dex10n@users.noreply.github.com>
2021-08-17 18:33:05 -0300

    feat: add email and password validation

--- js/login.js
+++ js/login.js
@@ -34,3 +34,59 @@ alertX.addEventListener("click", function () {
 successX.addEventListener("click", function () {
     snackbarSuccess.classList.add("hide");
 });
+
+
+// email and password validation
+const validateEmail = email => /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/.test(email);
+
+const email = document.querySelector(".input-box__text-box");
+const password = document.querySelector(".input-box__text-box--password");
+const iconBoxUsr = document.querySelector(".input-box__icon-box");
+const iconBoxKey = Array.from(document.querySelectorAll('.input-box__icon-box')).pop();
+const iconUsr = document.querySelector(".input-box__icon");
+const iconKey = document.querySelector('.input-box__icon--key');
+const emailErrorMsg = document.querySelector(".input__error-message");
+const passErrorMsg = Array.from(document.querySelectorAll('.input__error-message')).pop();
+const form = document.querySelector('.login__form');
+
+const addEmailErrorStyles = () => {
+    iconBoxUsr.classList.add('border-red');
+    iconUsr.classList.add('icon-red');
+    email.classList.add('border-red', 'text-red');
+    emailErrorMsg.classList.remove('vis-hidden');
+};
+
+const addPassErrorStyles = () => {
+    iconBoxKey.classList.add('border-red');
+    iconKey.classList.add('icon-red');
+    password.classList.add('border-red', 'text-red');
+    passErrorMsg.classList.remove('vis-hidden');
+};
+
+const removeEmailErrorStyles = () => {
+    iconBoxUsr.classList.remove('border-red');
+    iconUsr.classList.remove('icon-red');
+    email.classList.remove('border-red', 'text-red');
+    emailErrorMsg.classList.add('vis-hidden');
+};
+
+const removePassErrorStyles = () => {
+    iconBoxKey.classList.remove('border-red');
+    iconKey.classList.remove('icon-red');
+    password.classList.remove('border-red', 'text-red');
+    passErrorMsg.classList.add('vis-hidden');
+};
+
+email.addEventListener('click', removeEmailErrorStyles);
+password.addEventListener('click', removePassErrorStyles);
+
+form.addEventListener('submit', e => {
+    if(!validateEmail(email.value) || email.value.trim() === '') {
+        e.preventDefault();
+        addEmailErrorStyles();
+    }
+    if(password.value.length < 6) {
+        e.preventDefault();
+        addPassErrorStyles();
+    }
+});
